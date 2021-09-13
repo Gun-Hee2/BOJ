02b3c61112515a46261c479309031848ac6169ea
Gun-Hee2 <alg75@naver.com>
2021-09-14 00:06:20 +0900

    Upload Boj Code

---
+++ BOJ10952.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+public class BOJ10952 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+			if(num1 == 0 && num2 == 0) {
+				break;
+			}
+			System.out.println(num1 + num2);
+		}
+	}
+}
