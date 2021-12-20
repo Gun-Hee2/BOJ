6b4283e4fbfa67e4fb219a3796a5c899d8339dae
Gun-Hee2 <alg75@naver.com>
2021-12-20 21:56:03 +0900

    Update BOJ Code

---
+++ BOJ11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ11654 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		char ascc = scan.next().charAt(0);
+		
+		int asci = (int)ascc;
+		
+		System.out.println(asci);
+	}
+}
