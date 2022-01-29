a367ff5706d58b5bcda408f97c700a279487140d
Gun-Hee2 <alg75@naver.com>
2022-01-29 17:25:04 +0900

    Update BOJ Code

---
+++ BOJ1008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ1008 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextInt();
+		double b = scan.nextInt();
+		
+		System.out.println(a/b);
+	}
+}
