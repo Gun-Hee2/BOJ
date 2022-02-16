1fcced6112e6a4670d38df26d55ae29d637d1d8c
Gun-Hee2 <alg75@naver.com>
2022-02-16 19:36:01 +0900

    Update BOJ Code

---
+++ BOJ10926.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ10926 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String id = scan.next();
+		
+		System.out.println(id + "??!");
+
+	}
+
+}
